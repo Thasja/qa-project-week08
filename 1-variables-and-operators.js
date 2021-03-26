d12b011af0e12c9700ca1dd8d17b9141c11623e7
Thasja <wietjiskeja@gmail.com>
2021-03-25 23:35:40 -0300

    commit first exercise

--- 1-variables-and-operators.js
+++ 1-variables-and-operators.js
@@ -26,3 +26,9 @@ console.log(colorVehicle);
  * Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
  * **********************************************************************************/
 
+var flower, plant;
+flower = 'rose';
+plant = 'aloe vera';
+lengthStrings = flower.length + plant.length;
+
+console.log('The length of the 2 strings together is: ' + lengthStrings);
