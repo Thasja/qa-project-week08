0279bb73e9dc86a8bddd080d9a139849b0ee4eed
Thasja <wietjiskeja@gmail.com>
2021-03-28 19:01:59 -0300

    commit 5th exercise, cosmetic changes to the other exercises and updated the readme

--- 4-if-else.js
+++ 4-if-else.js
@@ -1,3 +1,5 @@
+//If-else
+
 /************************************************************************************
  * Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”
  * **********************************************************************************/
@@ -10,6 +12,13 @@ if(Math.random() >= 0.5){
 
 /************************************************************************************
  * Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+ “Bebe” si la edad es menor a 2 años
+ “Nino” si la edad es entre 2 y 12 años
+ “Adolecente” si la edad es entre 13 y 19 años
+ “Joven” si la edad está entre 20 y 30 años
+ “Adulto” entre 31 y 60 años
+ “Adulto mayor” entre 61 y 75 años
+ “Anciano” si es mayor a 75 años 
  * **********************************************************************************/
 
 var Age = 76;
@@ -25,7 +34,7 @@ if(Age < 2){
 } else if (Age <= 60){
     alert('Adulto');
 } else if (Age <= 75){
-alert('Adulto mayor');
+    alert('Adulto mayor');
 } else if (Age > 75){
-alert('Anciano');
+    alert('Anciano');
 }
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 **This repository contains 5 exercises to practise Javascript.**
 
 This is a learing project of Radium Rocket - Become a QA automation - to exercise Javascipt.
-This compository contains 5 exercises and each exercise is made on a separate .html file with its corresponding .js file.
+This compository contains 5 exercises and each exercise is made on a separate .js file linked to 1 basic .html file.
 
 ---
 
